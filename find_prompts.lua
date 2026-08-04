9209befe3bb3f5e449f0c07285847b83fd8d3ec4
ImNesk <ImNesk@users.noreply.github.com>
2026-08-05 00:34:35 +0200

    Remove unrelated project files from repo

--- find_prompts.lua
+++
@@ -1,5 +0,0 @@
-for _, v in ipairs(workspace:GetDescendants()) do
-    if v:IsA("ProximityPrompt") then
-        print("PROMPT:", v.Parent.Name, "| Objeto:", v.Parent.Parent and v.Parent.Parent.Name)
-    end
-end
